e0c9675bf9da595c3c49cd784f08ff9f71bd2a61
bnja_ <ben.monsalve@duocuc.cl>
2024-09-20 18:47:26 -0300

    primer_avance

---
+++ src/app/interfaces/menulink.ts
@@ -0,0 +1,6 @@
+export interface Menulink {
+
+    link:string,
+    icono:string,
+    label:string
+}
---
+++ src/app/pages/conductores/conductores.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conductores',
+  templateUrl: './conductores.page.html',
+  styleUrls: ['./conductores.page.scss'],
+})
+export class ConductoresPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
